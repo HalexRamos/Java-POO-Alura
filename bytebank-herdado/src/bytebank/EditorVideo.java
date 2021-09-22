343537b8b9eb748f298e4bec60e5a3ed642cc709
Halex Ramos <halexramos96@gmail.com>
2021-09-22 12:55:53 -0300

    polymorphism

---
+++ bytebank-herdado/src/bytebank/EditorVideo.java
@@ -0,0 +1,8 @@
+package bytebank;
+
+public class EditorVideo extends Funcionario {
+    public double getBonificacao() {
+        System.out.println("Chamando o metodo de bonificacao do EDITOR DE VIDEO");
+        return 150;
+    }
+}
